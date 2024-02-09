3d4635b20ca316953e7f8d75f8f6faef34538b3e
Udodirim <Udodirimwisdom@gmail.com>
2024-02-09 14:19:48 +0100

    Loop progress

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-async function start() {
+const template = document.querySelector("#pet-card-template");
+const wrapper = document.createDocumentFragment();
 
-    const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
-    const weatherData = await weatherPromise.json()
-    const ourTemp = weatherData.properties.periods[0].temperature
+async function start() {
+    const weatherPromise = await fetch(
+        "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
+    );
+    const weatherData = await weatherPromise.json();
+    const ourTemp = weatherData.properties.periods[0].temperature;
 
     document.querySelector("#temp-output").textContent = ourTemp;
 }
@@ -10,11 +14,23 @@ async function start() {
 start();
 
 async function petsArea() {
-    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
-    const petData = await petsPromise.json()
+    const petsPromise = await fetch(
+        "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
+    );
+    const petData = await petsPromise.json();
 
     petData.forEach((pet) => {
-        console.log(pet.name)
-    })
+        const clone = template.content.cloneNode(true);
+
+        clone.querySelector("h3").textContent = pet.name
+        // clone.querySelector("#pet-age").textContent = pet.birthYear
+        // clone.querySelector("#pet-description").textContent = pet.name
+        // clone.querySelector("img").textContent = pet.photo
+
+
+        wrapper.appendChild(clone)
+    });
+
+    document.querySelector(".list-of-pets").appendChild(wrapper);
 }
 petsArea();
